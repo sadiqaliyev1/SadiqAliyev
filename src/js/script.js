ccc11b9b9ecd1062abbef771a7687f6c222e8771
Sadiq <sadiqaliyev221@gmail.com>
2025-06-05 11:54:58 +0400

    initial commit

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".hamburgerInput");
+const hamburgerOpenNav = document.querySelector(".hamburgerOpenNav");
+hamburger.addEventListener("click",()=>{
+
+    hamburgerOpenNav.classList.toggle("open")
+
+    console.log("sa");
+})
+
+const hamburgerCloseNav = document.querySelector(".hamburgerCloseNav")
+
+
+
