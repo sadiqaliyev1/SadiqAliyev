8e9a67806d6899e6b2def8fe7b3f3845eba132f5
Sadiq <sadiqaliyev221@gmail.com>
2025-06-06 22:14:24 +0400

    create about page

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+   const flex  = document.querySelector(".flex");
+   const block  = document.querySelector(".educationInfo");
+   const none  = document.querySelector("none");
+
+   
    const hamburger = document.querySelector(".hamburgerInput");
         const hamburgerOpenNav = document.querySelector(".hamburgerOpenNav");
         hamburger.addEventListener("click", () => {
@@ -10,3 +15,37 @@
         const hamburgerCloseNav = document.querySelector(".hamburgerCloseNav")
 
 
+//! about
+  const items = document.querySelectorAll(".CvSelected div");
+  items.forEach(item => {
+    item.addEventListener("click", () => {
+      items.forEach(el => el.classList.remove("selected"));
+      item.classList.add("selected");
+    });
+  });
+
+
+// !Short Course
+const educationInfo = document.querySelector(".educationInfo");
+const ShortCourse = document.querySelector(".ShortCourse");
+const education = document.querySelector(".education");
+const educationText  = document.querySelector(".educationText");
+const shortCourseText = document.querySelector(".shortCourseText")
+// education.addEventListener("click",()=>{
+//     ShortCourse.classList.add("none")
+// })
+
+education.addEventListener("click",()=>{
+    educationInfo.style.display = "block"
+    ShortCourse.style.display = "none"
+    
+})
+
+shortCourseText.addEventListener("click",()=>{
+    educationInfo.style.display =  "none"
+    ShortCourse.style.display = "block"
+})
+
+// !Short Course
+
+//! about
